eee5e0374cfff4e39bde110d68001bca1d6d3364
Sealtiel <sealtielsw@hotmail.com>
2021-03-10 22:49:34 -0600

    Se agrego boton de prueba para conexión usando SQL Server

--- ConexionASQLServer/Form1.cs
+++ ConexionASQLServer/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DATOS;
 
 namespace ConexionASQLServer
 {
@@ -15,6 +17,15 @@ namespace ConexionASQLServer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Shana\source\repos\ConexionASQLServer\ConexionASQLServer\SampleDataBase.mdf;Integrated Security=True");
+            conexion.Open();
+            MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos");
+            conexion.Close();
+            MessageBox.Show("Se cerró la conexión.");
         }
     }
 }
