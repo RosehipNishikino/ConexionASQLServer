630bd833b065c62cc1c3887909949b0d259ab258
Sealtiel <sealtielsw@hotmail.com>
2021-03-10 22:42:13 -0600

    Se agrego la clase Conexión

---
+++ DATOS/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DATOS
+{
+    public class Conexion
+    {
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Shana\source\repos\ConexionASQLServer\ConexionASQLServer\SampleDataBase.mdf;Integrated Security=True");
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
