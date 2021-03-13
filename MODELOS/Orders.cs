f00042f3b13653ca3dac07feeb30119e7e59fe83
Sealtiel <sealtielsw@hotmail.com>
2021-03-12 20:38:23 -0600

    Los modelos Table y Orders se han agregado

---
+++ MODELOS/Orders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODELOS
+{
+    public class Orders
+    {
+        public int OrderID { get; set; }
+        public char CustomerID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int OrderQuantity { get; set; }
+        public Orders() { }
+    }
+}
---
+++ MODELOS/Table.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODELOS
+{
+    public class Table
+    {
+        public char CustomerID { get; set; }
+        public String CompanyName { get; set; }
+        public String ContactName { get; set; }
+        public String Phone { get; set; }
+        public Table() { }
+    }
+}
